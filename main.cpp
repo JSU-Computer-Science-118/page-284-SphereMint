b1ecdc9e827ffa6dc9ebaf0e73f6e9f5709c681b
SphereMint <83474707+SphereMint@users.noreply.github.com>
2022-11-28 11:10:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-// write your code here
+//Gannon Moore J00965925 11-28-2022
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int randCount;
+	cin >> randCount;
+
+	while (randCount > 0) {
+		cout << (rand() % 100) + 1 << endl;
+		randCount--;
+	}
+}
